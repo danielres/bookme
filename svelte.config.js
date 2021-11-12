8f235a50ba97d43fda6c7fae4da3c4d59adb2fe7
Daniel Reszka <dannn.r@gmail.com>
2021-11-12 02:48:55 +0100

    setup adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
+import adapterVercel from '@sveltejs/adapter-vercel'
+
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
@@ -13,6 +15,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: adapterVercel(),
   },
 }
 
